f2c6a899a9c0edac30c9e0caad559ce7f101efef
tnlthanzeel <tnlthanzeel@gmail.com>
2017-06-12 17:27:16 +0530

    init

---
+++ paymate/test/home.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="test.home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    running home azure
+</asp:Content>
---
+++ paymate/test/login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace test
+{
+    public partial class login : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            loginerror.Visible = false;
+        }
+
+        protected void btnlog_Click(object sender, EventArgs e)
+        {
+            string username = inputEmail.Value.ToString();
+            string pwd = password.Value.ToString();
+
+            if (pwd.Equals("asd"))
+            {
+
+                Response.Redirect("home.aspx");
+
+            }
+            else
+            {
+                loginerror.Visible = true;
+                pwd = "";
+                this.Dispose();
+            }
+        }
+    }
+}

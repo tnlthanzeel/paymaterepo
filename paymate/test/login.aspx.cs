9fd512db8d9bb744f0b006461b8bb6dab37b3043
Thanzeel <tnlthanzeel@gmail.com>
2017-09-17 15:41:14 +0530

    sql exceptions handled

--- paymate/test/login.aspx.cs
+++ paymate/test/login.aspx.cs
@@ -7,6 +7,7 @@ namespace test
 {
     public partial class login : System.Web.UI.Page
     {
+        bool retries = false;
         Customer c = new Customer();
         Admin admin = new Admin();
         private string conString = "";
@@ -20,50 +21,65 @@ namespace test
 
         protected void btnlog_Click(object sender, EventArgs e)
         {
-            string username = inputEmail.Value.ToString();
-            string pwd = password.Value.ToString();
 
-
-            SqlConnection con = new SqlConnection(conString);
-
-            SqlCommand cmd = new SqlCommand("select * from customer where dcusid='" + username + "'", con);
-
-            con.Open();
-            SqlDataReader rdr = cmd.ExecuteReader();
-
-            if (rdr.Read())
+            retry:
+            try
             {
-
-                string uname = rdr[1].ToString();
-                string upwd = rdr[2].ToString();
-                int userrole = Convert.ToInt16(rdr[3]);
-                int blockstat = Convert.ToInt16(rdr[4]);
-                var cusname = rdr[5].ToString();
+                string username = inputEmail.Value.ToString();
+                string pwd = password.Value.ToString();
 
 
+                SqlConnection con = new SqlConnection(conString);
 
-                Session["cusid"] = uname;
-                Session["loggedinas"] = cusname;
+                SqlCommand cmd = new SqlCommand("select * from customer where dcusid='" + username + "'", con);
 
-                if (pwd.Equals(upwd) && username.Equals(uname) && userrole == 1) // if userrole is 1, that user is an admin
-                {
-                    bool result = admin.login();
-                    Response.Redirect("Adminview.aspx");
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
 
-                }
-
-                else if (pwd.Equals(upwd) && username.Equals(uname) && userrole == 2 && blockstat == 0) //if userrole is 2, that user is an profile user, and if blockstat is 0 the customer is not blocked by the admin
-                {
-                    bool result = c.clogin();
-                    if (result == true)
-                        Response.Redirect("home.aspx");
-
-                }
-
-                else if (pwd.Equals(upwd) && username.Equals(uname) && userrole == 2 && blockstat == 1) //if userrole is 2, that user is an profile user, and if blockstat is 1 the customer is blocked by the admin
+                if (rdr.Read())
                 {
 
-                    userblocked.Visible = true;
+                    string uname = rdr[1].ToString();
+                    string upwd = rdr[2].ToString();
+                    int userrole = Convert.ToInt16(rdr[3]);
+                    int blockstat = Convert.ToInt16(rdr[4]);
+                    var cusname = rdr[5].ToString();
+
+
+
+                    Session["cusid"] = uname;
+                    Session["loggedinas"] = cusname;
+
+                    if (pwd.Equals(upwd) && username.Equals(uname) && userrole == 1) // if userrole is 1, that user is an admin
+                    {
+                        bool result = admin.login();
+                        Response.Redirect("Adminview.aspx");
+
+                    }
+
+                    else if (pwd.Equals(upwd) && username.Equals(uname) && userrole == 2 && blockstat == 0) //if userrole is 2, that user is an profile user, and if blockstat is 0 the customer is not blocked by the admin
+                    {
+                        bool result = c.clogin();
+                        if (result == true)
+                            Response.Redirect("home.aspx");
+
+                    }
+
+                    else if (pwd.Equals(upwd) && username.Equals(uname) && userrole == 2 && blockstat == 1) //if userrole is 2, that user is an profile user, and if blockstat is 1 the customer is blocked by the admin
+                    {
+
+                        userblocked.Visible = true;
+
+                    }
+
+
+                    else
+                    {
+                        loginerror.Visible = true;
+
+                        Dispose();
+                        pwd = "";
+                    }
 
                 }
 
@@ -76,18 +92,22 @@ namespace test
                     pwd = "";
                 }
 
+                con.Close();
+
             }
-
-
-            else
+            catch (Exception)
             {
-                loginerror.Visible = true;
+                if (retries == false)
+                {
+                    retries = true;
+                    goto retry;
+                }
+                Response.Redirect("login.aspx");
 
-                Dispose();
-                pwd = "";
             }
 
-            con.Close();
+
+
 
         }
     }
